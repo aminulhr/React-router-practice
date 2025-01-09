407358cc7026032ee9a3ace91ec4beed9bbbab8b
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-01-09 18:41:53 +0600

    set up router

---
+++ app/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-6xl bg-red-600">Lets Explore React Router</h1>
+    </div>
+  );
+};
+
+export default Home;
